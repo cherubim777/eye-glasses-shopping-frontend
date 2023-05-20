4354335f57b6a4a44d9527ca2f56a5fa95c67335
Nahom Abraham <nahomabraham17@gmail.com>
2023-05-20 12:30:28 +0300

    add new, popular and featured images in intro section, add stylings to it

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,6 +8,13 @@ export default function Home(){
                 <div className="intro-text-description">Discover a range of stylish and affordable eyeglasses that provide the vision care you deserve, all from the comfort of your own home.</div>
                 <button>Explore</button>
             </div>
+            <div className="intro-images">
+                <div className="intro-images-vertical">
+                    <div className="intro-image-n">new</div>
+                    <div className="intro-image-p">popular</div>
+                </div>
+                <div className="intro-image-f">featured</div>
+            </div>
         </div>
 
     )
