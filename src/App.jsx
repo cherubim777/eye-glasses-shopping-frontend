c5a31452abc22b5af5a6aaff45e6d5acee19335a
Nahom Abraham <nahomabraham17@gmail.com>
2023-05-20 11:38:00 +0300

    add home page component, add intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
 
   return (
+    <>
     <Navbar />
+    <Home />    
+    </>
   )
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home(){
+    return (
+        <div className="intro">
+            <div className="intro-text">
+                <div className="intro-text-title">Vision care for you</div>
+                <div className="intro-text-description">Discover a range of stylish and affordable eyeglasses that provide the vision care you deserve, all from the comfort of your own home.</div>
+                <button>Explore</button>
+            </div>
+        </div>
+
+    )
+}
