bc1534a2aa276985227dabdc6f6b8e41865baadc
yared <yaredte45@gmail.com>
2023-05-25 19:08:07 +0300

    style the right part of the login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,22 +3,24 @@ import UserInput from "./UserInput";
 export default function Login(){
 
     return (
-        <div>
-            <div className="login-form">
-                <div className="navbar-logo">VISION</div>
-                <div className="welcome">Welcome Back!!!</div>
-                <div className="logIn">Log in</div>
-                <UserInput type="email" title="Email"/>
-                <div className="link-style forgot-password-link ">Forgot Password?</div>
-                <UserInput type="password" title="Password"/>
-                <button className="button-style theme-color">Log In <img src="src/assets/arrow.png"/></button>
-                <div className="signup">
-                    <span className="signup-label">Don't have an account ? </span>
-                    <span className="signup-link link-style">Sign Up</span>
+        <div className="login-page">
+            <div className="login-left">
+                <div className="login-form">
+                    <div className="navbar-logo">VISION</div>
+                    <div className="welcome">Welcome Back!!!</div>
+                    <div className="logIn">Log in</div>
+                    <UserInput type="email" title="Email"/>
+                    <div className="link-style forgot-password-link ">Forgot Password?</div>
+                    <UserInput type="password" title="Password"/>
+                    <button className="button-style theme-color login-btn">Log In <img className="login-btn-arrow"src="src/assets/arrow.png"/></button>
+                    <div className="signup">
+                        <span className="signup-label">Don't have an account ? </span>
+                        <span className="signup-link link-style">Sign Up</span>
+                    </div>
                 </div>
-                </div>
+            </div>
                 
-            <img src="src/assets/login-image.png"/>
+            <img className="login-image" src="src/assets/login-image.png"/>
         </div>
 
     )
