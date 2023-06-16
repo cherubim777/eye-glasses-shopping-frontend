23b9bcf72d414d187752919d33f71cec257c0bb7
yared <yaredte45@gmail.com>
2023-06-16 09:48:01 +0300

    home page now get it's product from database

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,42 +1,49 @@
-import React from "react";
-import Navbar from "./Navbar";
-import Products from "./Products";
-import Footer from "./Footer";
+import React, { useEffect, useState } from 'react';
+import Navbar from './Navbar';
+import Products from './Products';
+import Footer from './Footer';
 
+export default function Home() {
+  const [products, setProducts] = useState([]);
 
-export default function Home(){
-    return (
-        <>
-        <Navbar user="customer"/>
-        <div className="intro">
-            <div className="intro-text">
-                <div className="intro-text-title">Vision care for you</div>
-                <div className="intro-text-description">Discover a range of stylish and affordable eyeglasses that provide the vision care you deserve, all from the comfort of your own home.</div>
-                <button className="theme-color button-style">Explore</button>
-            </div>
-            <div className="intro-images">
-                <div className="intro-images-vertical">
-                    <div className="intro-image-n">
-                        <div className="intro-image-tag theme-color">New</div>
-                    </div>
-                    <div className="intro-image-p">
-                        <div className="intro-image-tag theme-color">Popular</div>
-                    </div>
-                </div>
-                <div className="intro-image-f">
-                    <div className="intro-image-tag theme-color">Featured</div>
-                </div>
-            </div>
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error(error));
+  }, []);
 
+  return (
+    <>
+      <Navbar user="customer" />
+      <div className="intro">
+        <div className="intro-text">
+          <div className="intro-text-title">Vision care for you</div>
+          <div className="intro-text-description">
+            Discover a range of stylish and affordable eyeglasses that provide the vision care you deserve, all from the comfort of your own home.
+          </div>
+          <button className="theme-color button-style">Explore</button>
         </div>
-        
-        <Products category="Featured"/>
-        <Products category="New"/>
-        <Products category="Popular"/>
-        
-        <Footer />
-        
+        <div className="intro-images">
+          <div className="intro-images-vertical">
+            <div className="intro-image-n">
+              <div className="intro-image-tag theme-color">New</div>
+            </div>
+            <div className="intro-image-p">
+              <div className="intro-image-tag theme-color">Popular</div>
+            </div>
+          </div>
+          <div className="intro-image-f">
+            <div className="intro-image-tag theme-color">Featured</div>
+          </div>
+        </div>
+      </div>
 
-        </>
-    )
+      <Products category="Featured" products={products.filter((product) => product.category === 'Featured')} />
+      <Products category="New" products={products.filter((product) => product.category === 'New')} />
+      <Products category="Popular" products={products.filter((product) => product.category === 'Popular')} />
+
+      <Footer />
+    </>
+  );
 }
