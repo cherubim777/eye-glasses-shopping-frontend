c53653a1253b3ec157c9191d385b45a801eca726
yared <yaredte45@gmail.com>
2023-06-17 12:26:35 +0300

    now images are coming with correct url on allproducts page

--- src/Product.jsx
+++ src/Product.jsx
@@ -25,7 +25,7 @@ export default function Product(props){
     return (
         <Link to={`/customer/productDetails/${props.product.id}`}>
             <div className="product">
-            <img className="product-image" src={props.product.image} alt={props.product.name} />
+            <img className="product-image" src={`http://127.0.0.1:8000${props.product.photo}`} alt={props.product.name} />
             <div className="product-info">
                 <span className="product-name">{props.product.name}</span>
                 <span className="product-price">{props.product.price}</span>
