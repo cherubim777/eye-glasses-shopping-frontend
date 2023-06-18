6e6d35ae422218b2d79b6cd5bb1cd4dc065469c1
yared <yaredte45@gmail.com>
2023-06-18 10:58:18 +0300

    some modifications on wishlist(not finished)

--- src/WishList.jsx
+++ src/WishList.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import {useState,useEffect} from "react";
 import {useNavigate} from 'react-router-dom'
 import Item from "./Item";
 
 export default function WishList(){
+    const [wishListItems,setWishListItems] = useState([]);
+    
     const navigate = useNavigate()
+    const token = localStorage.getItem('CustomerToken');
+    
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/wishList/wishLists", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            }
+        }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+                setWishListItems(data.items);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }, []);
+
     return(
         <div className="cart">
             <p onClick={() => navigate(-1)} style={{cursor: "pointer"}}> &lt; Shopping Continue</p>
             <hr/>
             <p>Shopping Wishlist</p>
-            <p>You have 3 items in your wish List</p>
+            <p>You have {wishListItems.length} {wishListItems.length ==1?"item":"items"} in your wish List</p>
             <div className="cart-body">
                 <div className="cart-items">
-                    <Item user="customer"/>
-                    <Item user="customer"/>
-                    <Item user="customer"/>
+                    {wishListItems.map((item) => {
+                        return <><Item key={item.id} user="customer" {...item} />add to cart</>
+                   })}
                 </div>
                 
             </div>     
